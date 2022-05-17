f02080d0a0d97f442e6b1becd68e8d4b49e4a055
luis miguel la paz <nathaliemarie2303@gmail.com>
2022-05-17 19:55:55 -0400

    my project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SocialSecurityNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ssn;
+            string pattern = @"^(?!000)(?!666)(?!9[0-9][0-9])\d{3}[- ]?(?!00)\d{2}[- ]?(?!0000)\d{4}$";
+            Regex re= new Regex(pattern);
+            Console.WriteLine("Enter your SSN");
+            ssn = Console.ReadLine();
+            var Matching = re.IsMatch(ssn);
+            Console.WriteLine(Matching);
+
+            if (ssn == null)
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
